f483bb47e1a739507b393b294565229c38ad09fc
David Dyer <social_davidbdyer@icloud.com>
2024-07-24 19:30:48 -0400

    chore: Update index.html and app.js
    The index.html file was updated to add a meta tag for character encoding and a link to a CSS file. The title of the page was also changed to "Pirate Midi CLiCK Editor". Additionally, the buttons in the body section were given unique IDs for better accessibility.
    
    The app.js file was added, which contains JavaScript code for handling MIDI messages. It includes functions for sending MIDI messages based on button clicks and saving presets.
    
    This commit addresses the initial setup and functionality of the MIDI message sender.

---
+++ scripts/app.js
@@ -0,0 +1,47 @@
+const allOpen = document.querySelector('#all-open');
+const tipClosed = document.querySelector('#tip-closed');
+const ringClosed = document.querySelector('#ring-closed');
+
+const savePreset = document.querySelector('#save-preset');
+const presetNumber = document.querySelector('input', '#preset-number');
+
+
+savePreset.addEventListener('click',() => {
+	if (presetNumber.value >= 0 && presetNumber.value <= 127) {
+		sendMIDIMessage(3, presetNumber.value);
+	}
+});
+
+allOpen.addEventListener('click', () => sendMIDIMessage(2, 0));
+tipClosed.addEventListener('click', () => sendMIDIMessage(0, 64));
+ringClosed.addEventListener('click', () => sendMIDIMessage(1, 64))
+
+
+
+let midiAccess = null;
+
+navigator.requestMIDIAccess()
+	.then(onMIDISuccess, onMIDIFailure);
+
+function onMIDISuccess(access) {
+	midiAccess = access;
+	console.log('MIDI access obtained');
+}
+
+function onMIDIFailure() {
+	console.log('Could not access your MIDI devices.');
+}
+
+function sendMIDIMessage(control, value) {
+	if (midiAccess) {
+		const outputs = midiAccess.outputs.values();
+		for (let output of outputs) {
+			output.send([0xB0, control, value]); // CC message
+		}
+	}
+}
+
+
+
+
+
